d56e675d1a7078da9b71d0ae0a56d1f87b850cf9
charlottegrml <charlotte.gourmelond@gmail.com>
2026-03-11 16:46:19 +0100

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template_string, render_template, jsonify, request, redirect, url_for, session
 from flask import render_template
 from flask import json
+from tester.runner import run_tests
+from storage import save_run, list_runs
 from urllib.request import urlopen
 from werkzeug.utils import secure_filename
 import sqlite3
@@ -10,6 +12,21 @@ app = Flask(__name__)
 @app.get("/")
 def consignes():
      return render_template('consignes.html')
+
+
+@app.route("/run")
+def run():
+
+    result = run_tests()
+
+    save_run(result)
+
+    return jsonify(result)
+
+@app.route("/health")
+def health():
+
+    return {"status": "ok"}
 
 if __name__ == "__main__":
     # utile en local uniquement
